ceb6b45d7161149e36ec7ab52458bc7872e7a06d
Nayara Souza <nsouza@thoughtworks.com>
2020-10-10 23:05:00 -0300

    Adds google login button

--- src/pages/Login/Login.spec.tsx
+++ src/pages/Login/Login.spec.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
 import Login from './Login';
 import { LoginStrings } from './constants';
 
 describe('Login', () => {
-  it('render page with google login button', () => {
+  it('render google login button', () => {
+    (global.window as any).gapi = {
+      signin2: {
+        render: jest.fn(),
+      },
+    };
+
     render(<Login />);
 
     expect(
       screen.getByRole('heading', { name: LoginStrings.signInWithGoogle })
-    );
+    ).toBeInTheDocument();
+
+    act(() => {
+      global.window.dispatchEvent(new Event('google-loaded'));
+    });
+
+    expect((global.window as any).gapi.signin2.render).toHaveBeenCalled();
   });
 });
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { LoginStrings } from './constants';
 
+interface GoogleError {
+  error: string;
+  idpId: string;
+  type: string;
+}
+
+const loginSuccess = (googleUser: any) => {
+  const userProfile = googleUser.getBasicProfile();
+
+  console.log('userProfile :>> ', userProfile);
+};
+
+const loginFailed = (error: GoogleError) => console.log(error);
+
+const renderGoogleLoginButton = () => {
+  (window as any).gapi.signin2.render('login-button', {
+    width: 250,
+    height: 40,
+    theme: 'light',
+    onsuccess: loginSuccess,
+    onfailure: loginFailed,
+  });
+};
+
 const Login = () => {
-return <h1>{LoginStrings.signInWithGoogle}</h1>;
+  useEffect(() => {
+    window.addEventListener('google-loaded', () => renderGoogleLoginButton());
+  }, []);
+
+  return (
+    <>
+      <h1>{LoginStrings.signInWithGoogle}</h1>
+      <div id='login-button'></div>
+    </>
+  );
 };
 
 export default Login;
